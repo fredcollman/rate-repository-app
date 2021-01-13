59a5f04c4a4a8bae2026d78fdb7414425f359eb1
Fred Collman <fredcollman@gmail.com>
2021-01-13 17:51:23 +0000

    Add example form to help sign-in

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,30 @@
 import React from "react";
-
+import { Formik } from "formik";
+import { TouchableWithoutFeedback, View } from "react-native";
 import Text from "./Text";
+import FormikTextInput from "./FormikTextInput";
+
+const BodyMassIndexForm = ({ onSubmit }) => {
+  return (
+    <View>
+      <FormikTextInput name="mass" placeholder="Weight (kg)" />
+      <FormikTextInput name="height" placeholder="Height (m)" />
+      <TouchableWithoutFeedback onPress={onSubmit}>
+        <Text>Calculate</Text>
+      </TouchableWithoutFeedback>
+    </View>
+  );
+};
 
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
+  return (
+    <Formik
+      initialValues={{ mass: "", height: "" }}
+      onSubmit={(values) => console.log(values)}
+    >
+      {({ handleSubmit }) => <BodyMassIndexForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
 };
 
 export default SignIn;
